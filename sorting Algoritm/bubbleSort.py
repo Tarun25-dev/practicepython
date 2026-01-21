b21a1be79c8eb4d4c97c7c6c87119a79aba17148
Tarun25-dev <22x51a3225@srecnandyal.edu.in>
2026-01-21 18:16:03 +0530

    Update bubbleSort.py

--- sorting Algoritm/bubbleSort.py
+++ sorting Algoritm/bubbleSort.py
@@ -5,4 +5,8 @@ for i in range(len(li)):
     for j in range(0,len(li)-i-1):
         if li[j] > li[j+1]:
             li[j],li[j+1]=li[j+1],li[j]
+
 print(li)
+
+# Time complexity : O(n^2)
+# Space complexity : O(1)
