8cbd0c2ee0ae05cdc7297d967d3970426022ac07
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-02-20 22:27:53 +0530

    Add files via upload

---
+++ libraries/OS/workingWithFiles/getFileSize.py
@@ -0,0 +1,3 @@
+import os
+path = "C:\\Users\\THIS PC\\Desktop\\python\\OS\\Define.txt"
+print(os.path.getsize(path)) # 734 bytes means in kbs we need calculate like 1kb = 1024bytes so 734/1024 = 0.716kb
