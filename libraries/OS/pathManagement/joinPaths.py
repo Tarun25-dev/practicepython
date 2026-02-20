54fdf3f520771e2e90b22956f93bc83dafc8c454
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-02-20 22:23:36 +0530

    Add files via upload

---
+++ libraries/OS/pathManagement/joinPaths.py
@@ -0,0 +1,8 @@
+import os
+# syntax os.path.join(pathfolder,file)
+
+folder = "C:\\Users\\THIS PC\\Desktop\\python\\OS\\CreatedByOS"
+file_name = "data.txt"
+
+full_path = os.path.join(folder, file_name)
+print(full_path) # C:\Users\THIS PC\Desktop\python\OS\CreatedByOS\data.txt
