9abad7582f36be49fa00d303056472694d193304
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-02-21 16:37:23 +0530

    Update systemMonitor.py

--- systemMonitor.py
+++ systemMonitor.py
@@ -1,6 +1,7 @@
-import psutil,time,os
+import psutil,time,os # psutil(python system and process utilities) module used to get information about your susyem and running processes.
 while True:
     os.system("cls" if os.name=="nt" else "clear")
     print("CPU usage: ",psutil.cpu_percent(),"%")
     print("RAM usage: ",psutil.virtual_memory().percent,"%")
+
     time.sleep(1)
