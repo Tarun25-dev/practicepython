4d5192fa16d1ffbc056b7a884f63ff4dac3a64de
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-02-22 19:06:31 +0530

    Add files via upload

---
+++ Modules/sys/commandlineArgs.py
@@ -0,0 +1,8 @@
+# sys.argv() - used to take command line arguments
+import sys
+print(sys.argv)
+
+# type in command line 
+# python commandlineArgs.py tharun kumar
+# ['commandlineArgs.py', 'tharun', 'kumar']
+# useful for automation scripts
