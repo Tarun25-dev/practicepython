da108fd0b1d6192526917ad71a31a46659c99ba4
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-03-22 21:34:18 +0530

    Update 55.py

--- Practice/Practice2/55.py
+++ Practice/Practice2/55.py
@@ -1,1 +1,15 @@
-
+n=int(input())
+discount=0
+if n<=0:
+	print("Error")
+	exit()
+elif n<1000:
+	discount=5
+elif n>=1000 and n<=5000:
+	discount=10
+else:
+	discount=15
+d=discount/100*n
+pay=n-d
+print(int(pay))
+print("discount:",int(n-pay),"₹")
