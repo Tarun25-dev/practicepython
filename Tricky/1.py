3cf05a309d8d959af2cc28a7384f73d88193dd59
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-03-24 13:01:12 +0530

    Update 1.py

--- Tricky/1.py
+++ Tricky/1.py
@@ -1,1 +1,7 @@
-
+s="hello"
+t=s # it takes as refernces it t changes then automatically s also change 
+s=s.upper() #here we try to modify s by assigning same variable but py strings are immutable so it takes as a new variable that old variable s will automatically removes and then go to garbage
+print(t) #here t value is old s so already it stored old strings="hello"
+t=s # it takes refernce from s
+s=s.upper() 
+print(t)
