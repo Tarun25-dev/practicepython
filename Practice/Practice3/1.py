eb80fc3a5f73d1501de7bd191239244442078c9d
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-03-25 21:08:36 +0530

    Update 1.py

--- Practice/Practice3/1.py
+++ Practice/Practice3/1.py
@@ -1,1 +1,13 @@
+# "Write a program to read N key-value pairs from the user and print all keys in sorted order.
 
+n = int(input("Enter N: "))
+d = {}
+
+for _ in range(n):
+    key = input("Enter key: ")
+    value = input("Enter value: ")
+    d[key] = value
+
+dic=dict(sorted(d.items()))
+k=dic.keys()
+print(*k)
