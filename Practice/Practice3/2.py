b392645d7461c38b618af01e7278c9dc4013be4f
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-03-25 21:12:07 +0530

    Update 2.py

--- Practice/Practice3/2.py
+++ Practice/Practice3/2.py
@@ -38,3 +38,32 @@ Sample Output:
 
 1 5, 1 10, 2 3, 2 5, 3 7
 """
+
+n = int(input("Enter number of tasks: "))
+
+tasks = []
+
+# Input
+for _ in range(n):
+    p, t = map(int, input().split())
+    tasks.append([p, t])
+
+# Selection Sort
+for i in range(n):
+    min_idx = i
+    
+    for j in range(i + 1, n):
+        # Compare priority first
+        if tasks[j][0] < tasks[min_idx][0]:
+            min_idx = j
+        # If priority same, compare time
+        elif tasks[j][0] == tasks[min_idx][0]:
+            if tasks[j][1] < tasks[min_idx][1]:
+                min_idx = j
+    
+    # Swap
+    tasks[i], tasks[min_idx] = tasks[min_idx], tasks[i]
+
+# Output
+for task in tasks:
+    print(task[0], task[1], end=", ")
