ce836430186f4a863ef3f8e9fc1821e6fe9bae94
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-03-26 23:39:22 +0530

    Update 3.py

--- Practice/Practice3/3.py
+++ Practice/Practice3/3.py
@@ -2,11 +2,11 @@ Question: Dictionary + Sorting + Logic
 You are given a dictionary where:
 Keys are student names
 Values are marks (integers)
-👉 Your task:
+task:
 Sort the dictionary by marks in descending order
 If two students have same marks, sort them alphabetically by name
 Return only the names in the correct order
-🧠 Example Input:
+Example Input:
 Python
 Copy code
 {
@@ -15,7 +15,7 @@ Copy code
     "Ravi": 85,
     "Balu": 75
 }
-✅ Expected Output:
+Expected Output:
 Python
 Copy code
 ['Akhil', 'Ravi', 'Tarun', 'Balu']
