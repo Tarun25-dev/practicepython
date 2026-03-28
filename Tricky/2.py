cd50d8d1d487310bee81e690c7fa965fa962f9f0
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-03-28 22:55:41 +0530

    Implement function to find longest unique substring

--- Tricky/2.py
+++ Tricky/2.py
@@ -1,1 +1,16 @@
 # Longest Substring Without Repeating Characters
+
+def longest_unique_substring(s: str) -> int:
+    char_set = set()
+    left = 0
+    max_length = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+
+        char_set.add(s[right])
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
